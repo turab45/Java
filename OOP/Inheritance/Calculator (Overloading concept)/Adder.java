30aeeb694c950faa45c0f362d9feb27d79ca2801
Turaab Bajeer <50545053+turab45@users.noreply.github.com>
2020-02-23 00:54:56 +0500

    Add files via upload

---
+++ OOP/Inheritance/Calculator (Overloading concept)/Adder.java
@@ -0,0 +1,22 @@
+class Adder{
+	
+	public int add(int a,int b){
+		return a+b;
+	} 
+	
+	public int add(int a,int b,int c){
+		return a+b+c;
+	}
+	
+	public int add(int a,int b,int c,int d){
+		return a+b+c+d;
+	} 
+	
+	public int add(int a,int b,int c,int d,int e){
+		return a+b+c+d+e;
+	}
+	
+	public int add(int a,int b,int c,int d,int e,int f){
+		return a+b+c+d+e+f;
+	}
+}
---
+++ OOP/Inheritance/Calculator (Overloading concept)/Subtractor.java
@@ -0,0 +1,23 @@
+class Subtractor{
+	
+	public int sub(int a,int b){
+		return (a-b);
+	} 
+	
+	public int sub(int a,int b,int c){
+		return (a-b-c);
+	}
+	
+	public int sub(int a,int b,int c,int d){
+		return (a-b-c-d);
+	} 
+	
+	public int sub(int a,int b,int c,int d,int e){
+		return (a-b-c-d-e);
+	}
+	
+	public int sub(int a,int b,int c,int d,int e,int f){
+		return (a-b-c-d-e-f);
+	}
+	
+}

b00a69424f25d710aaa5ff40875a19501d0c35e3
Turaab Bajeer <50545053+turab45@users.noreply.github.com>
2020-04-11 11:29:51 +0500

    Create Department.java

---
+++ Collections/1. ArrayList/Student Department Management ( using generics)/Department.java
@@ -0,0 +1,16 @@
+public class Department {
+    String departmentName,departmentCode;
+
+    public Department(String departmentName, String departmentCode) {
+        this.departmentName = departmentName;
+        this.departmentCode = departmentCode;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public String getDepartmentCode() {
+        return departmentCode;
+    }
+}

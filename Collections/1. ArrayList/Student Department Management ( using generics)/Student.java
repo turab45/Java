694d6e076316c5be8e732dcf4a3082c72fe63f2c
Turaab Bajeer <50545053+turab45@users.noreply.github.com>
2020-04-11 11:29:22 +0500

    Create Student.java

---
+++ Collections/1. ArrayList/Student Department Management ( using generics)/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name, rollNumber;
+
+    public Student(String name, String rollNumber) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+}

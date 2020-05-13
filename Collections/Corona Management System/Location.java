eb32d6d7e090b55bc73b9ed944f8befb0638b5b2
Turaab Bajeer <50545053+turab45@users.noreply.github.com>
2020-05-13 12:46:59 +0500

    Create Location.java

---
+++ Collections/Corona Management System/Location.java
@@ -0,0 +1,27 @@
+public class Location {
+    private String taluka,district,province;
+
+    public void setTaluka(String taluka) {
+        this.taluka = taluka;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getTaluka() {
+        return taluka;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+}
